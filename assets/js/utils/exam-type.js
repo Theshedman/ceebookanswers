bfa953e31460700c997c4b6e642095a974a2b04c
Shedrack Ajaegbu <shedrack.ajaegbu@max.ng>
2020-05-17 18:04:06 +0100

    Add exam type utils

---
+++ assets/js/utils/exam-type.js
@@ -0,0 +1,12 @@
+export function examTypes() {
+  return (
+    `<option value="" selected="">Exam Type</option>
+     <option value="WAEC">WAEC</option>
+     <option value="NECO">NECO</option>
+     <option value="WAEC GCE 1ST SERIES">WAEC GCE 1ST SERIES</option>
+     <option value="WAEC GCE 2ND SERIES">WAEC GCE 2ND SERIES</option>
+     <option value="NECO GCE">NECO GCE</option>
+     <option value="NABTEB">NABTEB</option>
+     <option value="NABTEB GCE">NABTEB GCE</option>`
+  );
+}
