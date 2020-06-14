3e68c1851164c0f4f58e9443833d907d5886d994
Shedrack Ajaegbu <shedrack.ajaegbu@max.ng>
2020-06-14 16:26:47 +0100

    Update the request.js module to include delete and update functions

--- assets/js/utils/request.js
+++ assets/js/utils/request.js
@@ -37,6 +37,45 @@ async function postFormData(url, data) {
   }
 }
 
+async function update(url, data) {
+  try {
+    const response = await fetch(url, {
+      method: 'PATCH',
+      mode: 'cors',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${window.localStorage.getItem('token')}`
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(data)
+    });
+    return await response.json();
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+async function deleteData(url) {
+  try {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      mode: 'cors',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${window.localStorage.getItem('token')}`
+      },
+      redirect: 'follow',
+      referrerPolicy: 'no-referrer'
+    });
+    return await response.json();
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
 async function get(url) {
   try {
     const response = await fetch(url, {
@@ -56,4 +95,4 @@ async function get(url) {
   }
 }
 
-export { post, get, postFormData };
+export { post, get, postFormData, deleteData, update };
