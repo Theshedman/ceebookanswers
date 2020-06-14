e02979eca5a400d02b59a8454e5a7921b5deb183
Shedrack Ajaegbu <shedrack.ajaegbu@max.ng>
2020-06-14 16:25:55 +0100

    Remove the log statement in the post-answers.js file

--- assets/js/post-answers.js
+++ assets/js/post-answers.js
@@ -34,7 +34,6 @@ submitAnswer.addEventListener('click', async () => {
 
   try {
     const response = await postAnswer(formData);
-    console.log('response:', response);
     if (response.error) {
       throw new Error(response.error.message);
     }
