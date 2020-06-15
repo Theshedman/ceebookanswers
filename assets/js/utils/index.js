4bdfd9934d9ec592a2393ef60c996031f379e5ca
Shedrack Ajaegbu <shedrack.ajaegbu@max.ng>
2020-06-15 13:30:52 +0100

    Update default export file

--- assets/js/utils/index.js
+++ assets/js/utils/index.js
@@ -4,3 +4,5 @@ export * from './exam-year.js';
 export * from './subscription-type.js';
 export * from './request.js';
 export * from './alert.js';
+export * from './logout.js';
+export * from './change-password.js';
