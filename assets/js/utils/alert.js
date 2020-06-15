3b51d1536ddf233dc28812d22fd6c81f0f9d5755
Shedrack Ajaegbu <shedrack.ajaegbu@max.ng>
2020-06-15 12:46:05 +0100

    Update the alert module

--- assets/js/utils/alert.js
+++ assets/js/utils/alert.js
@@ -10,3 +10,24 @@ export const manipulateAlert = (alertElement, formElement) => {
     formElement.classList.remove('d-none');
   }, 3500);
 };
+
+export const manipulatePasswordUpdateElements = (alertElement, passwordBtn) => {
+  setTimeout(() => {
+    alertElement.classList.add('d-none');
+
+    passwordBtn.classList.add('d-flex');
+    passwordBtn.classList.remove('d-none');
+  }, 3500);
+};
+
+export const accessDeniedMessage = () => {
+  return `<div class="highlight-clean">
+    <div class="container">
+        <div class="intro">
+            <h2 class="text-center">Access Denied.</h2>
+            <p class="text-center text-danger">Please Login to view answers. You can only login in to one device. Logging in to another device will log you out from the first one. So make sure that you do not share your password with anyone to avoid being logged out from your device. </p>
+        </div>
+        </div>
+</div>
+`;
+}
