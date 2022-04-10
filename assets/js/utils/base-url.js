0be71f5580c4016b22e1d91e8ae1c893e52fe0dc
Shedrack Ajaegbu <shedrack3@gmail.com>
2022-04-10 16:31:40 +0100

    Update base-url.js

--- assets/js/utils/base-url.js
+++ assets/js/utils/base-url.js
@@ -1,3 +1,1 @@
-// export const baseUrl = `https://ceebookanswers.herokuapp.com`;
-// export const baseUrl = `http://63.250.44.66:3500`;
-export const baseUrl = `server2.ceebookanswers.net:3500`;
+export const baseUrl = `https://ceebookanswers.herokuapp.com`;
